dcc6abb3166195199faf1c7b17b8cbbbd144c8ed
Cristiane Foust <cristiane.foust@gmail.com>
2023-02-16 07:48:05 -0600

    Added a new fruit as a text input

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write("The user entered ", fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
@@ -43,4 +43,6 @@ streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.text("Thanks for adding jackfruit")
+streamlit.write("Thanks for adding ", add_my_fruit)
+
+
