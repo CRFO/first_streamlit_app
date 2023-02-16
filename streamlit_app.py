342ea9a1275b4058bdd7cccc4d942e7737bd7547
Cristiane Foust <cristiane.foust@gmail.com>
2023-02-15 21:57:46 -0600

    Added data frame to fruit load list

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.text("Thanks for adding jackfruit")
